df87fe4c57685822d2c32447941a6ac9ed543ae7
•wolf•™ <92360356+Wolfofficials@users.noreply.github.com>
2023-11-20 19:14:15 +0530

    Update join.py

--- join.py
+++ join.py
@@ -12,7 +12,7 @@ from telethon.tl.functions.messages import SendMessageRequest
 import sys
 logging.basicConfig(level=logging.INFO, handlers=[logging.StreamHandler(stream=sys.stdout)])
 logger = logging.getLogger(__name__)
-delay = random.randint (60 * 30, 60 * 180)
+delay = random.randint (1,3)#(60 * 30, 60 * 180)
 
 
 from creds import session, api_id, api_hash  # Import credentials from creds.py
@@ -36,19 +36,19 @@ async def join_group(group):
         # Attempt to join the group
         await client(JoinChannelRequest(group))
         log_and_send(logging.INFO, f'Joined chat ID: {group}')
+        await asyncio.sleep(delay)
     except FloodWaitError as e:
         log_and_send(logging.WARNING, f'Joining {group} failed due to flooding. Waiting for {e.seconds + delay} seconds...')
-        await asyncio.sleep(e.seconds)
+        await asyncio.sleep(e.seconds + delay)
     except Exception as e:
         log_and_send(logging.ERROR, f'Error joining chat ID: {group}. Details: {e}')
+        await asyncio.sleep(delay)
     finally:
         # Remove the group username from groups.txt immediately
         groups.remove(group)
         with open('groups.txt', 'w') as f:
             for g in groups:
                 f.write(f'{g}\n')
-                #await asyncio.sleep(delay)
-                await asyncio.sleep(delay)
 
 # Define send_log_message function
 async def send_log_message(message):
